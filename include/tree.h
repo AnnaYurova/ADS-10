a578bc49097f7e72cdd46f93be6e70737bc456db
AnnaYurova <94358663+AnnaYurova@users.noreply.github.com>
2022-06-02 22:35:01 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -12,57 +12,56 @@ class Tree {
     std::vector<Node*> sheet;
   };
   Node* root1;
-  std::vector<std::string> substitution;
-  void permutation(Node* root1, std::string sum = "") {
-    if (!root1 ->sheet.size()) {
-     sum += root1 -> number;
-    }
-    if (root1->number != '*') {
-      sum =+ root1->number;
-      substitution.push_back(sum);
-    }
-    if (root1->number != '*') {
-      sum += root1->number;
-    }
-    for (size_t i = 0; i < root1->sheet.size(); i++) {
-      permutation(root1->sheet[i], sum);
-    }
-  }
-  void newTree(Node* root1, std::vector<char> path) {
-    if (!path.size()) {
+  std::vector<std::string> subst;
+  void NewTree(Node* root1,  std::vector<char> NewFig) {
+    if (!NewFig.size()) {
       return;
     }
     if (root1->number != '*') {
-      for (auto i = path.begin(); i != path.end(); i++) {
-        if (*i == root1->number) {
-          path.erase(i);
+     for (auto x = NewFig.begin(); x != NewFig.end(); ++x) {
+        if (*x == root1->number) {
+          NewFig.erase(x);
           break;
         }
       }
     }
-    for (size_t i = 0; i < path.size(); i++) {
+    for (size_t i = 0; i < NewFig.size(); ++i) {
       root1->sheet.push_back(new Node);
     }
     for (size_t i = 0; i < root1->sheet.size(); ++i) {
-      root1->sheet[i]->number = path[i];
+      root1->sheet[i]->number = NewFig[i];
     }
     for (size_t i = 0; i < root1->sheet.size(); ++i) {
-      newTree(root1->sheet[i], path);
+      NewTree(root1->sheet[i], NewFig);
+    }
+  }
+  void Substitution(Node*root1, std::string s = "") {
+    if (!root1->sheet.size()) {
+      s += root1->number;
+      subst.push_back(s);
+      return;
+    }
+    if (root1->number != '*') {
+      s += root1->number;
+    }
+    for (size_t i = 0; i < root1->sheet.size(); ++i) {
+      Substitution(root1->sheet[i], s);
     }
   }
 
+
  public:
-  std::string operator[](int i) const {
-    if (i >= substitution.size()) {
-      return "";
-    }
-    return substitution[i];
-  }
-  explicit Tree(std::vector<char> number) {
+  explicit Tree(std::vector<char> b) {
     root1 = new Node();
     root1->number = '*';
-    newTree(root1, number);
-    permutation(root1);
+    NewTree(root1, b);
+    Substitution(root1);
+  }
+  std::string operator[](int i) const {
+    if (i >= subst.size()) {
+      return "";
+    }
+    return subst[i];
   }
 };
 
