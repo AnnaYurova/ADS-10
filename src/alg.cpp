825bb1b12c629c2ee0c01586f4c95e0f4e0493e6
AnnaYurova <94358663+AnnaYurova@users.noreply.github.com>
2022-06-02 22:22:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,7 +8,7 @@
 std::vector<char> getPerm(const Tree& tree, int n) {
   std::string s = tree[n-1];
   std::vector<char> newsubst;
-  for (int i = 0; i < s.length(); ++i) {
+  for (int i = 0; i < s.length(); i++) {
     newsubst.push_back(s[i]);
   }
   return newsubst;
